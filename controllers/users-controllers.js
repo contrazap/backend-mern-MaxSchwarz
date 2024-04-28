d394aff47b98083a09ae527659b16c5a1292c4a0
contrazap <contrazap@gmail.com>
2024-04-28 13:56:49 +0530

    backend

---
+++ controllers/users-controllers.js
@@ -0,0 +1,154 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
+
+const getUsers = async (req, res, next) => {
+  let users;
+
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong. Could not fetch users.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!users || users.length === 0) {
+    const error = new HttpError("Could not find any users.", 404);
+    return next(error);
+  }
+
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
+};
+
+const signup = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new HttpError(
+      "Invalid inputs passed, please check your data.",
+      422
+    );
+    return next(error);
+  }
+
+  const { name, email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    const error = new HttpError("Something went wrong. Signup failed.", 500);
+    return next(error);
+  }
+
+  if (existingUser) {
+    const error = new HttpError(
+      "User already exists. Please login instead.",
+      422
+    );
+    return next(error);
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user. Please try again.",
+      500
+    );
+    return next(error);
+  }
+
+  const createdUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+    image: req.file.path,
+    places: [],
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Something went wrong. Signup failed.", 500);
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Something went wrong. Signup failed.", 500);
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
+};
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    const error = new HttpError("Something went wrong. Login failed.", 500);
+    return next(error);
+  }
+
+  if (!existingUser) {
+    const error = new HttpError("Invalid credentials. Login failed!", 403);
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500
+    );
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError("Invalid credentials. Login failed!", 403);
+    return next(error);
+  }
+
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Something went wrong. Login failed.", 500);
+    return next(error);
+  }
+
+  res.json({
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
+  });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
---
+++ util/location.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+const secrets = require("../config/secrets");
+
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+const getCoordsForAddress = async (address) => {
+  // return {
+  //   lat: 40.7484445,
+  //   lng: -73.9882393,
+  // };
+
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+    address
+  )}&key=${API_KEY}`;
+  const response = await axios.get(url);
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for specified address.",
+      422
+    );
+    return error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+};
+
+module.exports = getCoordsForAddress;
